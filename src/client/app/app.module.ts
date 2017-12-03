80dea35120d1cf50f9c606db149318473f10dc3c
John Papa <john@Johns-MBP.lan>
2017-12-03 18:29:25 -0500

    freeze

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { environment } from '../environments/environment';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { StoreModule, Store } from '@ngrx/store';
+import { StoreModule, Store, MetaReducer } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
@@ -12,6 +12,10 @@ import { HeroService, HeroDataService, HeroEffects, reducers } from './store';
 import { HeroListComponent } from './hero-list.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
+import { storeFreeze } from 'ngrx-store-freeze';
+
+export const metaReducers: MetaReducer<any>[] = environment.production ? [] : [storeFreeze];
+
 @NgModule({
   declarations: [AppComponent, HeroListComponent, HeroDetailComponent],
   imports: [
@@ -19,14 +23,11 @@ import { HeroDetailComponent } from './hero-detail.component';
     FormsModule,
     // ReactiveFormsModule, // I could add this and then use FormControl with valueChanges
     HttpClientModule,
-    StoreModule.forRoot(reducers),
+    StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([HeroEffects]),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
-  providers: [
-    HeroDataService,
-    HeroService
-  ],
+  providers: [HeroDataService, HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}

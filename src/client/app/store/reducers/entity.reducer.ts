60df8c6fb86d56340feafa44064d4eba81e29c9b
John Papa <john@Johns-MBP.lan>
2017-12-06 16:24:26 -0500

    dead code

--- src/client/app/store/reducers/entity.reducer.ts
+++ src/client/app/store/reducers/entity.reducer.ts
@@ -1,6 +1,5 @@
 import { Hero } from '../../model';
 import * as fromActions from '../actions';
-import { Action } from '@ngrx/store';
 
 export interface EntityCache {
   // Must be any since we don't know what type of collections we will have

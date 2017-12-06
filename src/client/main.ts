9a81e679074bb4b2958a4c3d81fd903038ba88d2
Ward Bell <wardbell@hotmail.com>
2017-12-05 16:32:14 -0800

    feature: add angular-in-memory-web-api (#3)

--- src/client/main.ts
+++ src/client/main.ts
@@ -2,10 +2,10 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { AppDevModule } from './app/app-dev.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppDevModule);

d94aa6849f865fd3c2e383fa3b4f86759edcac3d
John Papa <john@Johns-MBP.jetpack>
2017-12-05 20:04:20 -0500

    prettier

--- src/client/app/store/actions/data.actions.ts
+++ src/client/app/store/actions/data.actions.ts
@@ -20,11 +20,11 @@ export abstract class DataErrorAction<T> implements Action {
 // that returns a function that returns
 // an observable of ngrx action(s) from DataService method observable
 export const toAction = (...actions: Action[]) => <T>(
-    source: Observable<T>,
-    successAction: new (data: T) => Action,
-    errorAction:   new (err: DataServiceError<T>) => Action
-  ) => source.pipe(
-    mergeMap((data: T) => [ new successAction(data), ...actions ]),
-    catchError((err: DataServiceError<T>) => of(new errorAction(err))),
+  source: Observable<T>,
+  successAction: new (data: T) => Action,
+  errorAction: new (err: DataServiceError<T>) => Action
+) =>
+  source.pipe(
+    mergeMap((data: T) => [new successAction(data), ...actions]),
+    catchError((err: DataServiceError<T>) => of(new errorAction(err)))
   );
-
--- src/client/app/store/effects/hero.effects.ts
+++ src/client/app/store/effects/hero.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import { concatMap, catchError, first, map, mergeMap, switchMap  } from 'rxjs/operators';
+import { concatMap, catchError, first, map, mergeMap, switchMap } from 'rxjs/operators';
 
 import { Action, Store } from '@ngrx/store';
 import { Actions, Effect } from '@ngrx/effects';
@@ -15,45 +15,60 @@ import { HeroicState } from '../reducers';
 
 const filterAction = new HeroActions.GetFilteredHeroes();
 const toAction = HeroActions.toAction(filterAction);
-type HeroAction = HeroActions.HeroAction
+type HeroAction = HeroActions.HeroAction;
 
 @Injectable()
 export class HeroEffects {
   @Effect()
   getHeroes$: Observable<Action> = this.actions$
-    .ofType(HeroActions.GET_HEROES).pipe(
-      switchMap(() => toAction(
-        this.heroDataService.getHeroes(),
-        HeroActions.GetHeroesSuccess, HeroActions.GetHeroesError
-      ))
+    .ofType(HeroActions.GET_HEROES)
+    .pipe(
+      switchMap(() =>
+        toAction(
+          this.heroDataService.getHeroes(),
+          HeroActions.GetHeroesSuccess,
+          HeroActions.GetHeroesError
+        )
+      )
     );
 
   @Effect()
   addHero$: Observable<Action> = this.actions$
-    .ofType(HeroActions.ADD_HERO).pipe(
-      concatMap((action: HeroAction) => toAction(
-        this.heroDataService.addHero(action.payload),
-        HeroActions.AddHeroSuccess, HeroActions.AddHeroError
-      ))
+    .ofType(HeroActions.ADD_HERO)
+    .pipe(
+      concatMap((action: HeroAction) =>
+        toAction(
+          this.heroDataService.addHero(action.payload),
+          HeroActions.AddHeroSuccess,
+          HeroActions.AddHeroError
+        )
+      )
     );
-
 
   @Effect()
   deleteHero$: Observable<Action> = this.actions$
-    .ofType(HeroActions.DELETE_HERO).pipe(
-      concatMap((action: HeroAction) => toAction(
-        this.heroDataService.deleteHero(action.payload),
-        HeroActions.DeleteHeroSuccess, HeroActions.DeleteHeroError
-      ))
+    .ofType(HeroActions.DELETE_HERO)
+    .pipe(
+      concatMap((action: HeroAction) =>
+        toAction(
+          this.heroDataService.deleteHero(action.payload),
+          HeroActions.DeleteHeroSuccess,
+          HeroActions.DeleteHeroError
+        )
+      )
     );
 
   @Effect()
   updateHero$: Observable<Action> = this.actions$
-    .ofType<HeroActions.UpdateHero>(HeroActions.UPDATE_HERO).pipe(
-      concatMap((action: HeroAction) => toAction(
-        this.heroDataService.updateHero(action.payload),
-        HeroActions.UpdateHeroSuccess, HeroActions.UpdateHeroError
-      ))
+    .ofType<HeroActions.UpdateHero>(HeroActions.UPDATE_HERO)
+    .pipe(
+      concatMap((action: HeroAction) =>
+        toAction(
+          this.heroDataService.updateHero(action.payload),
+          HeroActions.UpdateHeroSuccess,
+          HeroActions.UpdateHeroError
+        )
+      )
     );
 
   constructor(

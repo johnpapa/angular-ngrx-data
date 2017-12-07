838b675f5da6c2186b6fc747752b5e78ec5075e1
Ward Bell <wardbell@hotmail.com>
2017-12-07 11:58:45 -0800

    fixed local/remote; added local villains

--- src/client/app/shared/shared.module.ts
+++ src/client/app/shared/shared.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, HttpClientModule],
-  exports: [CommonModule, FormsModule, HttpClientModule],
+  imports: [CommonModule, FormsModule],
+  exports: [CommonModule, FormsModule],
   declarations: []
 })
 export class SharedModule {}
--- src/client/main.ts
+++ src/client/main.ts
@@ -8,5 +8,5 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-// platformBrowserDynamic().bootstrapModule(AppDevModule);
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppDevModule);
+// platformBrowserDynamic().bootstrapModule(AppModule);

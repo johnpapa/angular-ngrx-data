c8a8489af46ee2cf7cf45abb0a205eb18d6ccfd4
John Papa <john@Johns-MBP.lan>
2017-12-07 20:15:14 -0500

    removed unused files

--- src/client/app/store/effects/index.ts
+++
@@ -1,5 +0,0 @@
-import { HeroEffects } from './hero.effects';
-
-export const effects: any[] = [HeroEffects];
-
-export * from './hero.effects';
--- src/client/app/store/index.ts
+++ src/client/app/store/index.ts
@@ -1,4 +1,1 @@
-export * from './actions';
-export * from './effects';
-export * from './reducers';
 export * from './services';

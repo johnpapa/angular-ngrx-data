c9359e2ec4cea2a1419e74281a231688ef3315a6
John Papa <john@Johns-MBP.jetpack>
2017-12-08 14:04:31 -0500

    removed app-entites file and relocated it to the module

--- src/client/app/store/app-entity-store.module.ts
+++ src/client/app/store/app-entity-store.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { EntityEffects, EntityDataService, reducer } from '../../ngrx-data';
+import {
+  EntityCache,
+  EntityCollection,
+  EntityEffects,
+  EntityDataService,
+  initialEntityCollectionState,
+  reducer
+} from '../../ngrx-data';
 
 import { AppDataService, services } from './services';
-import { initialEntityCache } from './app-entities';
+import { Hero, Villain } from '../core';
 import { appConfigReducers } from './app-config/reducer';
 import { appConfigServices } from './app-config';
+
+const initialEntityCache: EntityCache = {
+  Hero: initialEntityCollectionState as EntityCollection<Hero>,
+  Villain: initialEntityCollectionState as EntityCollection<Villain>
+};
 
 @NgModule({
   imports: [

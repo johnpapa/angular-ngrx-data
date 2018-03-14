d63f470cb3fa7da894ef6735b616b8b2305d8a6c
Ward Bell <wardbell@hotmail.com>
2018-03-13 23:16:22 -0700

    refactor: simplify ngrx-data-toast.service

--- src/client/app/store/ngrx-data-toast.service.ts
+++ src/client/app/store/ngrx-data-toast.service.ts
@@ -1,8 +1,7 @@
 import { Injectable, OnDestroy } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import { filter, takeUntil, tap } from 'rxjs/operators';
+import { Subscription } from 'rxjs/Subscription';
 
 import { EntityAction, EntityActions, OP_ERROR, OP_SUCCESS } from 'ngrx-data';
 
@@ -11,19 +10,17 @@ import { ToastService } from '../core/toast.service';
 /** Report ngrx-data success/error actions as toast messages **/
 @Injectable()
 export class NgrxDataToastService implements OnDestroy {
-  private onDestroy = new Subject();
+  private subscription: Subscription;
 
   constructor(actions$: EntityActions, toast: ToastService) {
-    actions$.pipe(
-      filter(ea => ea.op.includes(OP_SUCCESS) || ea.op.includes(OP_ERROR)),
-      takeUntil(this.onDestroy)
-    )
+    this.subscription = actions$
+    .where(ea => ea.op.includes(OP_SUCCESS) || ea.op.includes(OP_ERROR))
     .subscribe(action =>
       toast.openSnackBar(`${action.entityName} action`, action.op)
     );
   }
 
   ngOnDestroy() {
-    this.onDestroy.next();
+    this.subscription.unsubscribe();
   }
 }

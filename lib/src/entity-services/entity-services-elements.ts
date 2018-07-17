4d53c27d5523cc97e311d8bc3030a754de27d062
Ward Bell <wardbell@hotmail.com>
2018-07-16 22:10:28 -0700

    fix: testability improvements for beta-10

--- lib/src/entity-services/entity-services-elements.ts
+++ lib/src/entity-services/entity-services-elements.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Action, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
+import { EntityAction } from '../actions/entity-action';
 import { EntityCache } from '../reducers/entity-cache';
 import { EntityDispatcherFactory } from '../dispatchers/entity-dispatcher-factory';
 import { EntitySelectors$Factory } from '../selectors/entity-selectors$';
@@ -16,10 +18,26 @@ export class EntityServicesElements {
      */
     public readonly entityCollectionServiceFactory: EntityCollectionServiceFactory,
     /** Creates EntityDispatchers for entity collections */
-    public readonly entityDispatcherFactory: EntityDispatcherFactory,
+    entityDispatcherFactory: EntityDispatcherFactory,
     /** Creates observable EntitySelectors$ for entity collections. */
-    public readonly entitySelectors$Factory: EntitySelectors$Factory,
+    entitySelectors$Factory: EntitySelectors$Factory,
     /** The ngrx store, scoped to the EntityCache */
     public readonly store: Store<EntityCache>
-  ) {}
+  ) {
+    this.entityActionErrors$ = entitySelectors$Factory.entityActionErrors$;
+    this.entityCache$ = entitySelectors$Factory.entityCache$;
+    this.reducedActions$ = entityDispatcherFactory.reducedActions$;
+  }
+
+  /** Observable of error EntityActions (e.g. QUERY_ALL_ERROR) for all entity types */
+  readonly entityActionErrors$: Observable<EntityAction>;
+
+  /** Observable of the entire entity cache */
+  readonly entityCache$: Observable<EntityCache> | Store<EntityCache>;
+
+  /**
+   * Actions scanned by the store after it processed them with reducers.
+   * A replay observable of the most recent action reduced by the store.
+   */
+  readonly reducedActions$: Observable<Action>;
 }

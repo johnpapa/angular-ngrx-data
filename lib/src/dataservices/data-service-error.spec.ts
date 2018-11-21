5b36da600adfe3506663b06807650fb963737361
Ward Bell <wardbell@hotmail.com>
2018-11-20 20:18:36 -0800

    refactor: DataServiceError for better message (beta.3)

---
+++ lib/src/dataservices/data-service-error.spec.ts
@@ -0,0 +1,32 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { DataServiceError } from './data-service-error';
+import { RequestData } from './interfaces';
+
+describe('DataServiceError', () => {
+  describe('#message', () => {
+    it('should define message when ctor error is string', () => {
+      const expected = 'The error';
+      const dse = new DataServiceError(expected, null);
+      expect(dse.message).toBe(expected);
+    });
+
+    it('should define message when ctor error is new Error("message")', () => {
+      const expected = 'The error';
+      const dse = new DataServiceError(new Error(expected), null);
+      expect(dse.message).toBe(expected);
+    });
+
+    it('should define message when ctor error is typical HttpResponseError', () => {
+      const expected = 'The error';
+      const body = expected; // server error is typically in the body of the server response
+      const httpErr = new HttpErrorResponse({
+        status: 400,
+        statusText: 'Bad Request',
+        url: 'http://foo.com/bad',
+        error: body
+      });
+      const dse = new DataServiceError(httpErr, null);
+      expect(dse.message).toBe(expected);
+    });
+  });
+});
